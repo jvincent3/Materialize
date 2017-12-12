9f77abfa419ce8b02b1fd57db2c8dd90ba647841
John Vincent Uy Calacsan <johnvuc@hotmail.com>
2017-12-12 13:45:15 +0100

    Release

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function() {
+    $(".button-collapse").sideNav();
+    $(".carousel.carousel-slider").carousel({
+        fullWidth: true,
+        indicators: true,
+        duration: 400,
+    })
+    $(".parallax").parallax();
+    $(".modal").modal({
+        opacity:.7,
+        inDuration: 400,
+        outDuration: 400,
+    });
+})
